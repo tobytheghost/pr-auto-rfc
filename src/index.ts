87583ca360a97e2baf1ccf9dca0218b164ea09fe
Toby Gates <tobygates@pensionbee.com>
2024-01-18 17:54:27 +0000

    Update for cleaner comments

--- src/index.ts
+++ src/index.ts
@@ -23,46 +23,52 @@ import { getPullRequest } from "./queries/getPullRequest";
 
     const { body } = await getPullRequest({ octokit, owner, repo, number });
 
-    const [_, ...forms] = body.split("<!--- rfc-form -->");
+    const [_, ...forms] = body.split("<!--- r-form -->");
 
     if (!forms.length) return console.log("No forms found in PR body");
 
-    const checkedForms = forms.map((form) => {
-      const formFields = form.split("<!--- rfc-end -->");
-      return formFields.map((field) => {
-        const title = /# (.*)/.exec(field)?.[1];
-        const type = /<!--- rfc-input-(.*) -->/.exec(field)?.[1];
+    const checkedForms = forms
+      .map((form) => {
+        const formFields = form.split("<!--- r-input-");
+        return formFields.map((field) => {
+          const type = /(.*) -->/.exec(field)?.[1];
+          const title = /# (.*)/.exec(field)?.[1];
+          if (!type || !title) throw new Error("Invalid form field ${field}");
 
-        if (type === "checklist") {
-          const missingChecklistItems = field
-            .split("\n")
-            .filter((item) => item.startsWith("- [ ]"));
-          if (!missingChecklistItems.length) return;
-          return `Please review and check the following items:\n${title}\n${missingChecklistItems.join(
-            "\n"
-          )}\n`;
-        }
+          if (type === "checklist") {
+            const missingChecklistItems = field
+              .split("\n")
+              .filter((item) => item.startsWith("- [ ]"));
+            if (!missingChecklistItems.length) return;
+            return `Please review and check the following items:\n${title}\n${missingChecklistItems.join(
+              "\n"
+            )}\n`;
+          }
 
-        if (type === "radio") {
-          const missingRadioItems = field
-            .split("\n")
-            .filter((item) => item.startsWith("- [ ]"));
-          if (!missingRadioItems.length) return;
-          return `Please review and check at least one of the following items:\n${title}\n${missingRadioItems.join(
-            "\n"
-          )}\n`;
-        }
+          if (type === "radio") {
+            const missingRadioItems = field
+              .split("\n")
+              .filter((item) => item.startsWith("- [ ]"));
+            if (!missingRadioItems.length) return;
+            return `Please review and check at least one of the following items:\n${title}\n${missingRadioItems.join(
+              "\n"
+            )}\n`;
+          }
 
-        if (type === "text") {
-          // Get value, remove title, remove comments & trim
-          const value = field.split(`${title}\r\n`)[1].replace(/<!--- (.*?) -->/gi, "").trim();
-          if(value.length)return;
-          return `Please fill in the following field: "${title}"\n`;
-        }
+          if (type === "text") {
+            // Get value, remove title, remove comments & trim
+            const value = field
+              .split(`${title}`)[1]
+              .replace(/<!--- (.*?) -->/gi, "")
+              .trim();
+            if (value.length) return;
+            return `Please fill in the following field: "${title}"\n`;
+          }
 
-        return;
-      });
-    }).reduce((acc, curr) => [...acc, ...curr], [] as string[]);
+          return;
+        });
+      })
+      .reduce((acc, curr) => [...acc, ...curr], [] as string[]);
 
     if (checkedForms.length) {
       throw new Error([...checkedForms].join("\n"));
